b3bd5b2852665e75bf9528aab5e865756ca338f5
Sonke Gumede <129692205+sonkeGumede@users.noreply.github.com>
2023-12-05 01:01:31 +0200

    feat: update main

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,10 +1,42 @@
+import BannerAlert from "../../components/Alert/banner-alert";
 import Banner from "../../components/Banner";
+import Reviews from "../../components/Banner/reviews";
 import Layout from "../../layout";
-
+import { addToCart, setProducts } from "../../store/product/reducer";
+import Product from "../Product";
+import Services from "../Services";
+import { useGetAllProductsQuery } from "./../../store/product/services";
+import { useAppDispatch } from "../../store/hooks";
+import { useEffect } from "react";
 const Main: React.FunctionComponent = () => {
+  const { data } = useGetAllProductsQuery({});
+  const disptach = useAppDispatch();
+
+  const handleCart = (id: number) => {
+    disptach(addToCart({ id }));
+  };
+
+  useEffect(() => {
+    disptach(setProducts(data));
+  }, [data]);
+
+  const newProducts = [...data || []].splice(5,4)
   return (
     <Layout>
       <Banner />
+      <Product
+        heading={"New Product"}
+        products={newProducts}
+        addToCart={handleCart}
+      />
+      <BannerAlert />
+      <Product
+        heading={"All Products"}
+        products={data}
+        addToCart={handleCart}
+      />
+      <Reviews />
+      <Services />
     </Layout>
   );
 };
