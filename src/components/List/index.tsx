eda3997b2aac4c0a84c77b57aa7d384ee12db7d3
Sonke Gumede <129692205+sonkeGumede@users.noreply.github.com>
2023-12-05 00:58:57 +0200

    feat: add list

---
+++ src/components/List/index.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+
+import Grid from "@mui/material/Grid";
+import CancelIcon from "../Icons/cancel-icon";
+import Divider from "@mui/material/Divider";
+
+const Count = styled("div")(`
+    width: 55px;
+    font-size: 12px;
+`);
+// const Demo = styled("div")(({ theme }) => ({
+//   backgroundColor: theme.palette.background.paper,
+// }));
+
+interface MappedProductProps {
+  image: string;
+  price: number;
+  id: number;
+  title: string;
+}
+
+interface InteractiveListProps {
+  products: MappedProductProps[];
+  onDelete: (id:number) => void;
+}
+
+export default function InteractiveList({ products , onDelete}: InteractiveListProps) {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item lg={12}>
+          <List dense={dense}>
+            {Object.entries(products).map(([key, value]: any) => {
+              const title = Object.keys(value)[0];
+              const image = value[title][0].image;
+              const count = value[title].length;
+              return (
+                <>
+                  <ListItem
+                    secondaryAction={
+                      <IconButton edge="end" aria-label="delete" onClick={()=>onDelete(value[title][0].id)}>
+                        <CancelIcon />
+                      </IconButton>
+                    }
+                  >
+                    <Count>{count} X </Count>
+                    <ListItemAvatar>
+                      <Avatar variant="square" src={image}></Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={title}
+                      secondary={secondary ? "Secondary text" : null}
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                </>
+              );
+            })}
+          </List>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
