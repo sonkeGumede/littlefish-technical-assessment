78c1a9f2a5cde62f686a13161939134224a5cee6
Sonke Gumede <129692205+sonkeGumede@users.noreply.github.com>
2023-12-04 02:57:57 +0200

    feat: setup theme

---
+++ src/theme/index.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#020202",
+    },
+    secondary: {
+      main: "#CACDD8",
+      light: "#FFFFFF",
+      dark: "#ACACAC",
+    },
+    info: {
+      main: "#0156FF",
+    },
+  },
+});
